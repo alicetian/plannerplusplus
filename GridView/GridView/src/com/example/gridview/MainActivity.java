8a8799bbeb0f0eb17be2bfac6581a9cfd698fe23
Alex Nassif <alex11578@gmail.com>
2014-05-20 17:37:56 -0700

    testing commit on git

---
+++ GridView/GridView/src/com/example/gridview/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.gridview;
+
+
+
+import android.support.v7.app.ActionBarActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+
+		public void onCreate(Bundle savedInstanceState) {
+		    super.onCreate(savedInstanceState);
+		    setContentView(R.layout.activity_main);
+		    
+		    GridView gridview = (GridView) findViewById(R.id.gridview);
+		    gridview.setAdapter(new ImageAdapter(this));
+
+		    gridview.setOnItemClickListener(new OnItemClickListener() {
+		        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+		        	Intent intent = new Intent(MainActivity.this, ShowCalendar.class);
+			     	   
+		     	    startActivity(intent);
+		            
+		            
+		        }
+		    });
+		}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	
+
+}
---
+++ GridView/GridView/src/com/example/gridview/ShowCalendar.java
@@ -0,0 +1,89 @@
+package com.example.gridview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.os.Build;
+
+public class ShowCalendar extends ActionBarActivity {
+	
+	private ListView list;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_calendar);
+		ActionBar actionBar = getSupportActionBar();
+	    
+	    actionBar.setDisplayUseLogoEnabled(false);
+	    actionBar.setDisplayHomeAsUpEnabled(true); 
+		
+		list = (ListView) findViewById(R.id.listView);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		        "Linux", "OS/2" };
+	
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+		list.setAdapter(adapter);
+		
+		
+		
+		
+	}
+	
+	@Override
+	public Intent getSupportParentActivityIntent(){
+		
+		
+		return new Intent(this, MainActivity.class);
+	    
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.show_calendar, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		else if (id == R.id.action_add){
+			
+			Intent intent = new Intent(this, AddEvent.class);
+			startActivity(intent);
+			
+			
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	
+
+}
